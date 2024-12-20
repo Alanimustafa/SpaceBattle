0abe8cb022c7b8c8e23a3ab6f36192e6c449cbb8
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-20 12:55:58 -0600

    308A | Space Battle | ScoreCounter Function has been created

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// SPace Battle | Game Container |
+class SpaceShip {
+  constructor (playerName, color, lives) {
+    this.playerName = playerName,
+    this.color = color,
+    this.lives = lives
+  }
+}
+
+
+
+
+
+
+ playerNumber = Number (prompt('enter a number'));
+
+  computerScore = 0;
+  computerLives = 3;
+for (let i = 0; i <= 100; i++) {
+  computerNumber = Math.floor(Math.random(i)*15);
+  if( computerNumber !== playerNumber) { 
+    console.log(playerNumber, computerNumber, 'Missed');
+  } else 
+  if ( computerNumber === playerNumber) {
+    console.log(Math.floor(Math.random(i)*15), playerNumber)
+    console.log(playerNumber, computerNumber,'......................................Decrement the computer life');
+    for (computerLives = 3; computerLives >= 0 ; computerLives --) {
+      computerScore -- ;
+      if (computerScore === 0) {
+        console.log('You Won! Game Over');
+      }
+    }
+    break;
+  }
+}
+
