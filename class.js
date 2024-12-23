3933c3581bdc9184a5ca22be561ec560a0d66c48
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-22 21:22:27 -0600

    Player Space Ship and Fire has been created

--- class.js
+++ class.js
@@ -13,7 +13,7 @@ class SpaceShip {
   }
 }
 
-// Space Ship Box details
+// Space Ship PC Box details
 let pcSpaceShip = new SpaceShip (
   50,  // Start X Location 
   50,  // Start Y Location 
@@ -23,9 +23,8 @@ let pcSpaceShip = new SpaceShip (
   2    // Y increments
 ) 
 
-console.log(pcSpaceShip.startX, pcSpaceShip.startY);
   
-// Fire Box details
+// Fire PC Box details
 let pcFire = new SpaceShip (
    50, // Fire Start X Position 
    50, // Fire Start Y Position 
@@ -35,9 +34,29 @@ let pcFire = new SpaceShip (
    5      // Fire Movement | Y Direction (moving downwards)
 );
 
-console.log(pcFire.startX,pcFire.yDirection)
 
-// Load the Ship image
+// Space Ship PLayer Box details
+let playerSpaceShip = new SpaceShip (
+  450,  // Start X Location 
+  650,  // Start Y Location 
+  70,  // The Ship width
+  100, // The Ship height
+  2,   // X increments 
+  2    // Y increments
+) 
+
+
+// Fire Player Box details
+let playerFire = new SpaceShip (
+  450, // Fire Start X Position 
+  650, // Fire Start Y Position 
+  70,   // Fire Size | Width
+  100, // Fire Size | Height
+  0,     // Fire Movement | X Direction
+  - 5      // Fire Movement | Y Direction (moving downwards)
+);
+
+// Load the PC Ship image
 const image1 = new Image();
 image1.src = './images/Rocket.png'; // The Space Ship image
 
@@ -45,11 +64,23 @@ image1.src = './images/Rocket.png'; // The Space Ship image
 const image2 = new Image();
 image2.src = './images/fire.png'; // The Fire image 
 
+// Load the Player Ship image
+const image3 = new Image();
+image3.src = './images/Rocket2.png'; // The Space Ship image
+
+// Load the Fire image
+const image4 = new Image();
+image4.src = './images/playerfire.png'; // The Fire image 
+
 // Draws the pcSpaceShip and fire
 function drawspaceShip() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-  ctx.drawImage(image1, pcSpaceShip.startX, pcSpaceShip.startY, pcSpaceShip.width, pcSpaceShip.height); // Draw the SpaceShip
-  ctx.drawImage(image2, pcFire.startX, pcFire.startY, pcFire.width, pcFire.height); // Draw the fire
+  ctx.drawImage(image1, pcSpaceShip.startX, pcSpaceShip.startY, pcSpaceShip.width, pcSpaceShip.height); // Draw the PC SpaceShip
+  ctx.drawImage(image2, pcFire.startX, pcFire.startY, pcFire.width, pcFire.height); // Draw the PC fire
+
+  ctx.drawImage(image3, playerSpaceShip.startX, playerSpaceShip.startY, playerSpaceShip.width, playerSpaceShip.height); // Draw the Player SpaceShip
+  ctx.drawImage(image4, playerFire.startX, playerFire.startY, playerFire.width, playerFire.height); // Draw the Player fire
+
 }
 
 // Update the pcSpaceShip and fire positions
