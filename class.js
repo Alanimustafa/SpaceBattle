86b9d1e76f9a7df4f71c1147d611e43bbceba208
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-22 20:17:40 -0600

    PC Player and PC fire have been switched to classes

--- class.js
+++ class.js
@@ -11,28 +11,34 @@ class SpaceShip {
     this.xDirection = xDirection,
     this.yDirection = yDirection
   }
+  test () {
+    console.log('Yes Im here');
+  }
 }
 
 // Space Ship Box details
-let spaceShip = { 
-  x: 50,       // Start X Location 
-  y: 50,       // Start Y Location 
-  width: 50,   // The Ship width
-  height: 100, // The Ship height
-  dx: 2,       // X increments 
-  dy: 2        // Y increments
-};
+let pcSpaceShip = new SpaceShip (
+  50,  // Start X Location 
+  50,  // Start Y Location 
+  50,  // The Ship width
+  100, // The Ship height
+  2,   // X increments 
+  2    // Y increments
+) 
 
+console.log(pcSpaceShip.startX, pcSpaceShip.startY, pcSpaceShip.test());
+  
 // Fire Box details
-let fire = {
-  firesSTARTX: 50, // Fire Start X Position 
-  firesSTARTY: 50, // Fire Start Y Position 
-  fireWidth: 70,   // Fire Size | Width
-  fireHeight: 100, // Fire Size | Height
-  fireXinc: 0,     // Fire Movement | X Direction
-  fireYinc: 5      // Fire Movement | Y Direction (moving downwards)
-};
+let fire = new SpaceShip (
+   50, // Fire Start X Position 
+   50, // Fire Start Y Position 
+   70,   // Fire Size | Width
+   100, // Fire Size | Height
+   0,     // Fire Movement | X Direction
+   5      // Fire Movement | Y Direction (moving downwards)
+);
 
+console.log(fire.startX,fire.width)
 // Load the Ship image
 const image1 = new Image();
 image1.src = './images/Rocket.png'; // The Space Ship image
@@ -41,47 +47,47 @@ image1.src = './images/Rocket.png'; // The Space Ship image
 const image2 = new Image();
 image2.src = './images/fire.png'; // The Fire image 
 
-// Draws the spaceShip and fire
+// Draws the pcSpaceShip and fire
 function drawspaceShip() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-  ctx.drawImage(image1, spaceShip.x, spaceShip.y, spaceShip.width, spaceShip.height); // Draw the SpaceShip
+  ctx.drawImage(image1, pcSpaceShip.x, pcSpaceShip.y, pcSpaceShip.width, pcSpaceShip.height); // Draw the SpaceShip
   ctx.drawImage(image2, fire.firesSTARTX, fire.firesSTARTY, fire.fireWidth, fire.fireHeight); // Draw the fire
 }
 
-// Update the spaceShip and fire positions
-function updatespaceShipPosition() {
-  // SpaceShip Movement
-  spaceShip.x += spaceShip.dx;
-  spaceShip.y += spaceShip.dy;
+// // Update the pcSpaceShip and fire positions
+// function updatespaceShipPosition() {
+//   // SpaceShip Movement
+//   pcSpaceShip.x += pcSpaceShip.dx;
+//   pcSpaceShip.y += pcSpaceShip.dy;
 
-  // Fire Movement | it will be only on Y direction to make the fire goes down
-  fire.firesSTARTY += fire.fireYinc; 
+//   // Fire Movement | it will be only on Y direction to make the fire goes down
+//   fire.firesSTARTY += fire.fireYinc; 
 
-  // Keeping the Space Ship within the canvas
-  if (spaceShip.x <= 0 || spaceShip.x + spaceShip.width >= canvas.width) {
-    spaceShip.dx *= -1; // Reverse x direction horizontally
-  }
-  if (spaceShip.y <= 0 || spaceShip.y + spaceShip.height >= canvas.height - 250) {
-    spaceShip.dy *= -1; // Reverse y direction vertically
-  }
+//   // Keeping the Space Ship within the canvas
+//   if (pcSpaceShip.x <= 0 || pcSpaceShip.x + pcSpaceShip.width >= canvas.width) {
+//     pcSpaceShip.dx *= -1; // Reverse x direction horizontally
+//   }
+//   if (pcSpaceShip.y <= 0 || pcSpaceShip.y + pcSpaceShip.height >= canvas.height - 250) {
+//     pcSpaceShip.dy *= -1; // Reverse y direction vertically
+//   }
 
-  // Reset fire position if it moves off the canvas
-  if (fire.firesSTARTY > canvas.height) {
-    fire.firesSTARTY = spaceShip.y + spaceShip.height / 2; // Reset fire Y position
-    fire.firesSTARTX = spaceShip.x + spaceShip.width / 2 - fire.fireWidth / 2; // Align fire X position with the spaceship center
-  }
-}
+//   // Reset fire position if it moves off the canvas
+//   if (fire.firesSTARTY > canvas.height) {
+//     fire.firesSTARTY = pcSpaceShip.y + pcSpaceShip.height / 2; // Reset fire Y position
+//     fire.firesSTARTX = pcSpaceShip.x + pcSpaceShip.width / 2 - fire.fireWidth / 2; // Align fire X position with the spaceship center
+//   }
+// }
 
-// Animation loop using fucntoin recursion.
-function animate() {
-  drawspaceShip();
-  updatespaceShipPosition();
-  requestAnimationFrame(animate); // Loop the animation
-}
+// // Animation loop using fucntoin recursion.
+// function animate() {
+//   drawspaceShip();
+//   updatespaceShipPosition();
+//   requestAnimationFrame(animate); // Loop the animation
+// }
 
-animate();
+// animate();
 
 
-// Start the animation once the images are loaded
-// image1.onload = animate;
-//image2.onload = animate;
+// // Start the animation once the images are loaded
+// // image1.onload = animate;
+// //image2.onload = animate;
